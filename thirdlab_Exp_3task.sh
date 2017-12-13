4dae6d9c2061c747b341dd15bd59cda217f5fe1f
Shchegoleva Angelina Ivanovna <shshegoleva@ccfit.nsu.ru>
2017-12-13 23:15:54 +0700

    3 task

---
+++ thirdlab_Exp_3task.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm E3task3.txt
+wget http://factorized.net/patterns.txt
+egrep -w "\b[A,B,E,K,M,H,O,P,C,T,Y,X][0-9]{3}[A,B,E,K,M,H,O,P,C,T,Y,X]{2}[0-9]{2,3}\b" patterns.txt >> E3task3.txt
+rm patterns.txt
