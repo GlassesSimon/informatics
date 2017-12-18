3d4b92a699f185ed822560aec37a1a00fd344485
Shchegoleva Angelina Ivanovna <shshegoleva@ccfit.nsu.ru>
2017-12-18 20:22:33 +0700

    NEW

--- thirdlab_Exp_2task.sh
+++ thirdlab_Exp_2task.sh
@@ -2,7 +2,7 @@
 rm E3task2.txt
 wget  http://factorized.net/patterns.txt
 echo "LINES WITH ONLY NUMBERS" >> E3task2.txt
-egrep -w "\b[0-9]*\b" patterns.txt >> E3task2.txt
+grep -w "\b[0-9]*\b" patterns.txt >> E3task2.txt
 echo "LINES WITH ONLY LETTERS" >> E3task2.txt
-egrep -w "\b[A-Za-z]*\b" patterns.txt >> E3task2.txt
+grep -w "\b[A-Za-z]*\b" patterns.txt >> E3task2.txt
 rm patterns.txt
