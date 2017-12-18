5dd0a69fddcde21776972a4304cb06e0dab73f9b
Shchegoleva Angelina Ivanovna <shshegoleva@ccfit.nsu.ru>
2017-12-18 22:22:44 +0700

    NEW

--- secondlab_Exp_4task.sh
+++ secondlab_Exp_4task.sh
@@ -6,5 +6,5 @@ chmod u=r,go= shared/
 chmod go=rwx shared/
 chmod g=rwx shared/
 chmod u=rwx shared/
-scp /home/students/17200/shshegoleva/informatics/shared/ beryanov@mephisto:/home/students/17200/beryanov/shared/
-scp /home/students/17200/shshegoleva/informatics/shared/ gavrilova@mephisto:/home/students/17200/gavrilova/shared/
+scp -r /home/students/17200/shshegoleva/informatics/shared/ beryanov@mephisto:/home/students/17200/beryanov/shared/
+scp -r /home/students/17200/shshegoleva/informatics/shared/ gavrilova@mephisto:/home/students/17200/gavrilova/shared/
