50e36460b15f663a34d645b5e9dc88db92493005
Shchegoleva Angelina Ivanovna <shshegoleva@ccfit.nsu.ru>
2017-12-18 20:16:33 +0700

    NEW

--- thirdlab_Exp_1task.sh
+++ thirdlab_Exp_1task.sh
@@ -2,11 +2,11 @@
 rm E3task1.txt
 wget http://factorized.net/crusoe.txt
 echo "WORD STARTS WITH 'Z'" >> E3task1.txt
-egrep -o "\bz\w*\b" crusoe.txt >> E3task1.txt 
+grep -o "\bz\w*\b" crusoe.txt >> E3task1.txt 
 echo "16 LETTERS IN WORD" >> E3task1.txt
-egrep -o "\b\w{16}\b" crusoe.txt >> E3task1.txt
+grep -o "\b\w\{16\}\b" crusoe.txt >> E3task1.txt
 echo "WORD STARTS WITH 'A' AND ENDS WITH 'C'" >> E3task1.txt
-egrep -o "\ba\w*c\b" crusoe.txt >> E3task1.txt
+grep -o "\ba\w*c\b" crusoe.txt >> E3task1.txt
 echo "WORDS STARTS WITH 'AB' AND 3rd LETTER ISN'T 'C'" >> E3task1.txt
-egrep -o "\bab[^o]\w*\b" crusoe.txt >> E3task1.txt
+grep -o "\bab[^o]\w*\b" crusoe.txt >> E3task1.txt
 rm crusoe.txt
